9c35686e8c601507563f4043906a2e522e072bd0
oc <2862431899@qq.com>
2021-09-28 18:12:26 +0800

    mybatis-plus

---
+++ orm-mybaits-plus/src/main/java/com/example/ormmybaitsplus/OrmMybaitsPlusApplication.java
@@ -0,0 +1,13 @@
+package com.example.ormmybaitsplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmMybaitsPlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmMybaitsPlusApplication.class, args);
+    }
+
+}
